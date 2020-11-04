7ed81a00aa162353a81bf84e90eb62078c1e735d
1625462 <1625462>
2020-11-04 17:17:49 +0800

    Recipe TEST_2.compute_model_pkl saved through notebook notebook editor for compute_model_pkl

--- recipes/compute_model_pkl.py
+++ recipes/compute_model_pkl.py
@@ -1,18 +1,35 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
-
-
+import xgboost as XGB
+import pickle
 # Read recipe inputs
 inner_cv = dataiku.Dataset("inner_cv")
-inner_cv_df = inner_cv.get_dataframe()
+df = inner_cv.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+model = XGB.XGBClassifier(learning_rate = 0.2)
 
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+leaveOut = ['client', 'ID', 'label', 'Date', 'Rank', 'category1']
+ftsRaw = [x for x in df.keys() if x not in leaveOut]
+lbCol = 'label'
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+dfdummy = pd.get_dummies(df['category1'])
+dftrain = pd.concat([df[ftsRaw + [lbCol]], dfdummy], axis = 1)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+fts = ftsRaw + list(dfdummy.keys())
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+X = dftrain[fts].values
+y = dftrain[lbCol].tolist()
+model.fit(X, y)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 model_pkl_df = inner_cv_df # For this sample code, simply copy input to output
 
 
