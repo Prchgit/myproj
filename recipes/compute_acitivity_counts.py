5b5ee96e0a3d10a321b22113e845f626ed06adcc
1625462 <1625462>
2020-11-04 09:57:29 +0800

    Recipe TEST_2.compute_acitivity_counts saved through notebook notebook editor for compute_acitivity_counts

--- recipes/compute_acitivity_counts.py
+++ recipes/compute_acitivity_counts.py
@@ -1,7 +1,9 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
+from collections import Counter
 
 # Read recipe inputs
 activity_data = dataiku.Dataset("activity_data")
@@ -14,7 +16,17 @@ activity_data_df = activity_data.get_dataframe()
 
 acitivity_counts_df = activity_data_df # For this sample code, simply copy input to output
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# acitivity_counts_df.keys()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+clients = dict(Counter(acitivity_counts_df['clients'].tolist()))
+dfActCount = pd.DataFrame.from_dict([{"client": x, 'count': y} for x, y in clients.items()])
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# dfActCount[0:3]
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 acitivity_counts = dataiku.Dataset("acitivity_counts")
-acitivity_counts.write_with_schema(acitivity_counts_df)
+acitivity_counts.write_with_schema(dfActCount)
