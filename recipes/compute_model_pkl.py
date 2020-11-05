629d861c8aacf8918e30201fafba826211344e7e
1625462 <1625462>
2020-11-05 11:57:20 +0800

    Recipe TEST_2.compute_model_pkl saved through notebook notebook editor for compute_model_pkl

--- recipes/compute_model_pkl.py
+++ recipes/compute_model_pkl.py
@@ -22,17 +22,24 @@ dfdummy = pd.get_dummies(df['category1'])
 dftrain = pd.concat([df[ftsRaw + [lbCol]], dfdummy], axis = 1)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+dftrain = dftrain.loc[dftrain[lbCol].notnull()]
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 fts = ftsRaw + list(dfdummy.keys())
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 X = dftrain[fts].values
 y = dftrain[lbCol].tolist()
 model.fit(X, y)
+folder = dataiku.Folder("qmPE2pZN")
+with folder.get_writer("xgbMd.pkl") as file_obj:
+    pickle.dump(model, file_obj)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-model_pkl_df = inner_cv_df # For this sample code, simply copy input to output
+# any(dftrain[lbCol].isnull())
 
-
-# Write recipe outputs
-model_pkl = dataiku.Dataset("model_pkl")
-model_pkl.write_with_schema(model_pkl_df)
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# model_pkl_df = inner_cv_df # For this sample code, simply copy input to output
+# # Write recipe outputs
+# model_pkl = dataiku.Dataset("model_pkl")
+# model_pkl.write_with_schema(model_pkl_df)
