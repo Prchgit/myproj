a949f08591bb0fe6fb8aa8f152ef2c3a30b9243b
1625462 <1625462>
2020-11-06 09:57:14 +0800

    Saved recipe: TEST_2.compute_model_pkl

--- recipes/compute_model_pkl.py
+++ recipes/compute_model_pkl.py
@@ -5,28 +5,23 @@ import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 import xgboost as XGB
 import pickle
+
 # Read recipe inputs
 inner_cv = dataiku.Dataset("inner_cv")
 df = inner_cv.get_dataframe()
-
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 model = XGB.XGBClassifier(learning_rate = 0.2)
-
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 leaveOut = ['client', 'ID', 'label', 'Date', 'Rank', 'category1']
 ftsRaw = [x for x in df.keys() if x not in leaveOut]
 lbCol = 'label'
-
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 dfdummy = pd.get_dummies(df['category1'])
 dftrain = pd.concat([df[ftsRaw + [lbCol]], dfdummy], axis = 1)
-
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 dftrain = dftrain.loc[dftrain[lbCol].notnull()]
-
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 fts = ftsRaw + list(dfdummy.keys())
-
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 X = dftrain[fts].values
 y = dftrain[lbCol].tolist()
@@ -34,10 +29,8 @@ model.fit(X, y)
 folder = dataiku.Folder("qmPE2pZN")
 with folder.get_writer("xgbMd.pkl") as file_obj:
     pickle.dump(model, file_obj)
-
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # any(dftrain[lbCol].isnull())
-
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # model_pkl_df = inner_cv_df # For this sample code, simply copy input to output
 # # Write recipe outputs
